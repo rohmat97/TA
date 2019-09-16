fc063291dce9c9a65e73727a6dd74e3c3fd5e067
rohmat97 <dasukirohmat@gmail.com>
2019-09-16 08:14:38 +0700

    a

--- ta/ba/Backend-Development-TA-master/src/main/java/com/kota101/innstant/service/FileWriterService.java
+++
@@ -1,9 +0,0 @@
-package com.kota101.innstant.service;
-
-import java.io.IOException;
-
-public interface FileWriterService {
-    void writeToFile(String stringToWrite) throws IOException;
-
-    void initFile() throws IOException;
-}
